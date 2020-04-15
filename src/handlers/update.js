2e13017e62405f55c1fc606aa61741ac966a2aee
unknown <james.jenour@protonmail.com>
2020-04-15 13:16:54 +1000

    10

--- src/handlers/update.js
+++ src/handlers/update.js
@@ -13,6 +13,23 @@ exports.updateHandler = async (event) => {
     // Get id and name from the body of the request
     const { id, name } = JSON.parse("{\"id\": \""+Date.now()+"\", \"name\": \"Test\"}");
 
+    var url = 'https://api.hypixel.net/skyblock/auctions?key=4f6867b2-b8af-437e-86b3-71a8259db905&page=1';
+
+    http.get(url, function(res){
+        var body = '';
+
+        res.on('data', function(chunk){
+            body += chunk;
+        });
+
+        res.on('end', function(){
+            var fbResponse = JSON.parse(body);
+            console.log("Got a response: ", fbResponse);
+        });
+    }).on('error', function(e){
+          console.log("Got an error: ", e);
+    });
+
     const params = {
         TableName: tableName,
         Item: { id, name },
