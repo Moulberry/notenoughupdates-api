e6a06a1628f7c4088fb6c2f89e5991f7947bd9cb
unknown <james.jenour@protonmail.com>
2020-04-16 08:41:46 +1000

    30

--- src/handlers/update.js
+++ src/handlers/update.js
@@ -24,54 +24,55 @@ exports.updateHandler = async (event) => {
 
 	let settings = { method: "Get" };
 
-	let auctions = []
 	let totalPages = 1;
 	let currentPages = 0;
 
 	await fetch(url+'0', settings)
 		.then(res => res.json())
-		.then((json) => {
+		.then(async (json) => {
 			totalPages = json.totalPages;
 			currentPages++;
-			auctions = auctions.concat(json.auctions);
+			await processAuctions(json.auctions)
 		});
 
+
 	for(var i=1; i<totalPages; i++) {
-		try {
-            fetch(url+i, settings)
-            .then(res => res.json())
-            .then((json) => {
-                auctions = auctions.concat(json.auctions);
-                currentPages++;
-            });
-        } catch(err) { console.log(err) }
+		fetch(url+i, settings)
+		.then(res => res.json())
+		.then(async (json) => {
+			await processAuctions(json.auctions)
+			currentPages++;
+		});
 	}
 
-	//Max 20s (400 sleeps)
+	//Max 40s (800 sleeps)
 	let sleeps = 0;
-	while(currentPages < totalPages && sleeps < 400) {
+	while(currentPages < totalPages && sleeps < 800) {
 		sleeps++;
 		await sleep(50);
 	}
-	console.log("GET Took: " + (Date.now() - start)/1000 + "s");
 
-	if(auctions[0] == undefined) console.log("Throtted.")
+    const response = {
+        statusCode: 200,
+        body: "{\"message\": \"Updated.\"}",
+    };
 
+	console.log("Took: " + (Date.now() - start)/1000 + "s");
+    return response;
+}
+
+async function processAuctions(auctions) {
 	var processed = {}
-
-	var toParse = 0;
-	var parsed = 0;
 	for(var i=0; i<auctions.length; i++) {
 		let auction = auctions[i]
-		if(auction != undefined && auction.highest_bid_amount > 0) {
-			toParse++;
-			nbt.parse(Buffer.from(auction.item_bytes, 'base64'), async function(error, data) {
+		if(auction != undefined && auction.highest_bid_amount > 0 && (auction.end-Date.now())/1000/60 < 2) {
+			await nbt.parse(Buffer.from(auction.item_bytes, 'base64'), async function(error, data) {
 				if (error) { throw error; }
 
 				try {
 					let enchantments = {}
 					let hot_potato_count = 0
-					let modifier = "none"
+					let modifier = ""
 					let count = data.value.i.value.value[0].Count.value;
 
 					let tag = data.value.i.value.value[0].tag.value;
@@ -80,55 +81,22 @@ exports.updateHandler = async (event) => {
 					try { hot_potato_count = ExtraAttributes.hot_potato_count.value; } catch(err) { }
 					try { modifier = ExtraAttributes.modifier.value; } catch(err) { }
 					try {
-						/*console.log(auction.uuid + "-" + auction.auctioneer)
-						//console.log(ExtraAttributes.id.value);
+						//console.log(auction.uuid + "-" + auction.auctioneer)
+						console.log(ExtraAttributes.id.value)
+						/*console.log(auction.highest_bid_amount);
 						console.log(count)
 						console.log(enchantments)
 						console.log(hot_potato_count)
 						console.log(modifier)*/
-						if(processed[ExtraAttributes.id.value] == undefined) {
-                            const params = {
-                                TableName: tableName,
-                                Key: { "id": ExtraAttributes.id.value },
-                            };
-                            const { Item } = await docClient.get(params).promise();
-                            if(Item == undefined || Item.value == undefined) {
-                                processed[ExtraAttributes.id.value] = {}
-                            } else {
-                                processed[ExtraAttributes.id.value] = Item.value
-                            }
-						}
-						processed[ExtraAttributes.id.value][auction.uuid + "-" + auction.auctioneer] =
-								{"bid": auction.highest_bid_amount, count, enchantments, hot_potato_count, modifier}
-					} catch(err) { }
+						const params = {
+                            TableName: tableName,
+                            Item: { id, auction.uuid + "-" + auction.auctioneer:
+                                {"bid": auction.highest_bid_amount, count, enchantments, hot_potato_count, modifier} },
+                        };
+                        await docClient.update(params).promise();
+					} catch(err) { console.log(err)}
 				} catch(err) {}
-				parsed++;
 			});
 		}
 	}
-	//Max 20s (400 sleeps)
-	sleeps = 0;
-	while(parsed < toParse && sleeps < 400) {
-		sleeps++;
-		await sleep(50);
-	}
-
-	console.log("PROCESS Took: " + (Date.now() - start)/1000 + "s");
-
-	for(id in processed) {
-	    console.log(id)
-	    const params = {
-            TableName: tableName,
-            Item: { id, "value": processed[id] },
-        };
-        await docClient.put(params).promise();
-	}
-
-    const response = {
-        statusCode: 200,
-        body: "{\"message\": \"Updated.\"}",
-    };
-
-	console.log("ALL Took: " + (Date.now() - start)/1000 + "s");
-    return response;
-};
+}
