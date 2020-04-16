43e274856746a7fbfb60fe2007d0420b6723b1b9
unknown <james.jenour@protonmail.com>
2020-04-16 13:49:01 +1000

    56

--- src/handlers/update.js
+++ src/handlers/update.js
@@ -237,7 +237,7 @@ exports.updateHandler = async (event) => {
 
     const setBazaarProcessIndexParams = {
         TableName: tableName,
-        IndexItem: { "id": "BAZAAR_PROCESS_INDEX", "value":  (index+20)%products.length},
+        IndexItem: { "id": "BAZAAR_PROCESS_INDEX", "value":  (startindex+20)%products.length},
     };
     await docClient.put(setBazaarProcessIndexParams).promise();
 
